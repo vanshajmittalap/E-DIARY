a88080792844b3c8ebefe427ce5824f9bad37a9e
Vanshaj Mittal <vanshajmittal1204@gmail.com>
2023-08-01 17:44:53 +0530

    Client

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -11,7 +11,7 @@ const Signup = (props) => {
   let history = useHistory();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    const response = await fetch("https://backend-ljql.onrender.com/api/auth/createuser", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
